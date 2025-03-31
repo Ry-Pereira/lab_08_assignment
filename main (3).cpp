229d180d2d2a646cec82045c7ebe2aa1833b82b2
Ryan <145312422+Ry-Pereira@users.noreply.github.com>
2025-03-30 19:04:01 -0500

    Fixed error with printing grid

--- main (3).cpp
+++ main (3).cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -29,11 +21,21 @@ public:
 	void store_value(int row,int column,int value) {
 		grid[row][column] = value;
 	}
+	void print_grid() {
+		for(int row= 0; row<number_of_rows; row++) {
+		    cout<< "Row"<<row;
+			for(int column = 0; column < number_of_columns; column++) {
+				cout<<grid[row][column]<< " ";
+			}
+			cout<<"\n";
+			
+		}
+	}
 
 };
 
-pair<Matrix,Matrix> create_matrices(string matrix_text_file){
-    string line;
+pair<Matrix,Matrix> create_matrices(string matrix_text_file) {
+	string line;
 	int n = 1;
 	int m=-1;
 	int j = 0;
@@ -68,7 +70,7 @@ pair<Matrix,Matrix> create_matrices(string matrix_text_file){
 						for(int i = 0; i<size_of_matrix; i++) {
 							std::getline(ss,s,' ');
 							int value = stoi(s);
-							matrix1.store_value(i,m,value);
+							matrix1.store_value(m,i,value);
 							cout<<"MS Row: "<<m<< " Column: "<<i<< " Value: "<<value << "\n";
 						}
 						if(m == size_of_matrix) {
@@ -76,11 +78,11 @@ pair<Matrix,Matrix> create_matrices(string matrix_text_file){
 							j= 0;
 						}
 					}
-					else{
+					else {
 						for(int i = 0; i<size_of_matrix; i++) {
 							std::getline(ss,s,' ');
 							int value = stoi(s);
-							matrix2.store_value(i,j,value);
+							matrix2.store_value(j,i,value);
 							cout<<"MR Row "<<j<< " Column: "<<i<< " Value: "<<value << "\n";
 						}
 						j++;
@@ -89,7 +91,7 @@ pair<Matrix,Matrix> create_matrices(string matrix_text_file){
 				}
 
 				m++;
-				
+
 			}
 			n++;
 		}
@@ -97,8 +99,8 @@ pair<Matrix,Matrix> create_matrices(string matrix_text_file){
 		return {matrix1,matrix2};
 	}
 }
-Matrix create_matrice(string matrix_text_file){
-    string line;
+Matrix create_matrice(string matrix_text_file) {
+	string line;
 	int n = 1;
 	int m=-1;
 	int j = 0;
@@ -126,9 +128,7 @@ Matrix create_matrice(string matrix_text_file){
 			else {
 				std::stringstream ss(line);
 				string s;
-
 				if(line.size()-1) {
-
 					if(m < size_of_matrix) {
 						for(int i = 0; i<size_of_matrix; i++) {
 							std::getline(ss,s,' ');
@@ -137,91 +137,220 @@ Matrix create_matrice(string matrix_text_file){
 							cout<<"MS Row: "<<m<< " Column: "<<i<< " Value: "<<value << "\n";
 						}
 					}
-
 				}
-
 				m++;
-				
 			}
 			n++;
 		}
 		myfile.close();
-		return {matrix1,matrix2};
+		return matrix1;
 	}
 }
 
-Matrix add_matrices(Matrix matrix_1,Matrix matrix_2){
-    Matrix result_matrix{matrix_1.number_of_rows,matrix_1.number_of_columns};
-    
-    for(int row=0;row<matrix_1.number_of_rows;row++){
-        for(int column = 0; column < matrix_1.number_of_columns;column){
-            int value = matrix_1.grid[row][column] + matrix_2.grid[row][column];
-            result_matrix.store_value(row,column,value);
-            
-        }
+Matrix add_matrices(Matrix matrix_1,Matrix matrix_2) {
+	Matrix result_matrix{matrix_1.number_of_rows,matrix_1.number_of_columns};
+
+	for(int row=0; row<matrix_1.number_of_rows; row++) {
+		for(int column = 0; column < matrix_1.number_of_columns; column) {
+			int value = matrix_1.grid[row][column] + matrix_2.grid[row][column];
+			result_matrix.store_value(row,column,value);
+
+		}
+
+	}
+
+	return result_matrix;
+}
+
+Matrix multiply_matrices(Matrix matrix_1,Matrix matrix_2) {
+	Matrix result_matrix{matrix_1.number_of_rows,matrix_1.number_of_columns};
+
+	for(int row=0; row<matrix_1.number_of_rows; row++) {
+		for(int column = 0; column < matrix_1.number_of_columns; column) {
+			for(int colu = 0; colu < matrix_2.number_of_columns; colu) {
+				int value = matrix_1.grid[row][colu] * matrix_2.grid[colu][row];
+				result_matrix.store_value(row,column,value);
+			}
+
+		}
+
+	}
+
+	return result_matrix;
+}
+
+void sum_diagonal(Matrix matrix_1) {
+	int d = 0;
+	int matrix_1_result_main_sum = 0;
+	int matrix_1_result_secondary_sum = 0;
+
+	Matrix result_matrix_1{matrix_1.number_of_rows,matrix_1.number_of_columns};
+
+	for(int row = 0; row < matrix_1.number_of_rows; row++) {
+		matrix_1_result_main_sum+= matrix_1.grid[row][d];
+		d++;
+	}
+	for(int row = matrix_1.number_of_rows; row > 0; row--) {
+		matrix_1_result_secondary_sum+= matrix_1.grid[row][d];
+		d--;
+	}
+
+	cout<< matrix_1_result_main_sum << "\n";
+	cout<< matrix_1_result_secondary_sum << "\n";
+
+
+}
+
+Matrix swap_rows(Matrix matrix_1,int row_num_1,int row_num_2) {
+	Matrix result_matrix = matrix_1;
+
+	if(row_num_1> matrix_1.number_of_rows) {
+		return result_matrix;
+	}
+	else if(row_num_2> matrix_1.number_of_rows) {
+		return result_matrix;
+	}
+	else if(row_num_1 < 0) {
+		return result_matrix;
+	}
+	else if(row_num_2 < 0) {
+		return result_matrix;
+	}
+	else {
+		for(int col = 0; col< matrix_1.number_of_rows; col++) {
+			int value = matrix_1.grid[row_num_2][col];
+			result_matrix.store_value(row_num_1,col,value);
+		}
+		for(int col = 0; col< matrix_1.number_of_rows; col++) {
+			int value = matrix_1.grid[row_num_1][col];
+			result_matrix.store_value(row_num_2,col,value);
+		}
+		return result_matrix;
+	}
+
+}
+Matrix swap_columns(Matrix matrix_1,int column_num_1,int column_num_2) {
+	Matrix result_matrix = matrix_1;
+
+	if(column_num_1> matrix_1.number_of_rows) {
+		return result_matrix;
+	}
+	else if(column_num_2> matrix_1.number_of_rows) {
+		return result_matrix;
+	}
+	else if(column_num_1 < 0) {
+		return result_matrix;
+	}
+	else if(column_num_2 < 0) {
+		return result_matrix;
+	}
+	else {
+		for(int row = 0; row< matrix_1.number_of_rows; row++) {
+			int value = matrix_1.grid[row][column_num_2];
+			result_matrix.store_value(column_num_1,row,value);
+		}
+		for(int row = 0; row< matrix_1.number_of_rows; row++) {
+			int value = matrix_1.grid[row][column_num_1];
+			result_matrix.store_value(column_num_2,row,value);
+		}
+		return result_matrix;
+	}
+
+}
+
+Matrix update_matrix(Matrix matrix,int row,int column, int value) {
+	matrix.store_value(row,column,value);
+	return matrix;
+}
+
+void print_menu() {
+	cout<<"1: Read values from a file into the matrix" << "\n";
+	cout<<"2: Add two matrices and display the result" << "\n";
+	cout<<"3: Multiply two matrices and display the result" << "\n";
+	cout<<"4: Get the sum of matrix diagonal elements" << "\n";
+	cout<<"5: Swap matrix rows and display the result" << "\n";
+	cout<<"6: Swap matrix columns and display the result" << "\n";
+	cout<<"7: Update matrix rows and display the result" << "\n";
+}
+void choice_selection() {
+	int choice;
+	cout<<"Choice: ";
+	cin  >> choice;
+	string matrix_numbers_text_file_name;
+	Matrix matrix1(0,0);
+	Matrix matrix2(0,0);
+	Matrix result(0,0);
+	Matrix matrix(0,0);
+
+
+	switch(choice) {
+	case 1:
+	{
+		cout<<"Enter Matrix Numbers Text File Name: ";
+		cin >> matrix_numbers_text_file_name;
+		pair<Matrix,Matrix> matrices = create_matrices(matrix_numbers_text_file_name);
+		matrix1 = matrices.first;
+		matrix2 = matrices.second;
         
-    }
-    
-    return result_matrix;
-}
+		cout<< "Matrix 1: \n";
+		cout<< matrix1.grid[0][0] << matrix1.grid[0][1];
+		break;
+	}
+	case 2:
+	{
+		cout<<"Enter Matrix Numbers Text File Name: ";
+		cin >> matrix_numbers_text_file_name;
+		pair<Matrix,Matrix> matrices = create_matrices(matrix_numbers_text_file_name);
+		matrix1 = matrices.first;
+		matrix2 = matrices.second;
+		result = add_matrices(matrix1,matrix2);
+		break;
+	}
 
-Matrix multiply_matrices(Matrix matrix_1,Matrix matrix_2){
-    Matrix result_matrix{matrix_1.number_of_rows,matrix_1.number_of_columns};
-    
-    for(int row=0;row<matrix_1.number_of_rows;row++){
-        for(int column = 0; column < matrix_1.number_of_columns;column){
-            for(int colu = 0; colu < matrix_2.number_of_columns;colu){
-            int value = matrix_1.grid[row][colu] * matrix_2.grid[colu][row];
-            result_matrix.store_value(row,column,value);
-            }
-            
-        }
-        
-    }
-    
-    return result_matrix;
-}
+	case 3:
+	{
+		cout<<"Enter Matrix Numbers Text File Name: ";
+		cin >> matrix_numbers_text_file_name;
+		pair<Matrix,Matrix> matrices = create_matrices(matrix_numbers_text_file_name);
+		matrix1 = matrices.first;
+		matrix2 = matrices.second;
+		result = multiply_matrices(matrix1,matrix2);
+		break;
+	}
+	case 4:
+	{
+		cout<<"Enter Matrix Numbers Text File Name: ";
+		matrix = create_matrice(matrix_numbers_text_file_name);
+		sum_diagonal(matrix);
+		break;
+	}
+	case 5:
+	{
+		cout<<"Enter Matrix Numbers Text File Name: ";
+		matrix = create_matrice(matrix_numbers_text_file_name);
+		sum_diagonal(matrix);
+		break;
+	}
+	case 6:
+	{
+		cout<<"Enter Matrix Numbers Text File Name: ";
+		matrix = create_matrice(matrix_numbers_text_file_name);
+		sum_diagonal(matrix);
+		break;
+	}
+	case 7:
+	{
+		cout<<"Enter Matrix Numbers Text File Name: ";
+		cin >> matrix_numbers_text_file_name;
+		break;
+	}
 
-void sum_diagonal(Matrix matrix_1,Matrix matrix_2){
-    int d = 0;
-    int matrix_1_result_main_sum = 0;
-    int matrix_1_result_secondary_sum = 0;
-    int matrix_2_result_main_sum = 0;
-    int matrix_2_result_secondary_sum = 0;
-    Matrix result_matrix_1{matrix_2.number_of_rows,matrix_2.number_of_columns};
-    Matrix result_matrix_2{matrix_2.number_of_rows,matrix_2.number_of_columns};
-    for(int row = 0; row < matrix_1.number_of_rows;row++;){
-        matrix_1_result_main_sum+= matrix_1.grid[row][d]
-        d++;
-    }
-    for(int row = matrix_1.number_of_rows; row > 0;row--;){
-        matrix_1_result_secondary_sum+= matrix_1.grid[row][d]
-        d--;
-    }
-    for(int row = 0; row < matrix_2.number_of_rows;row++;){
-        matrix_2_result_main_sum+= matrix_2.grid[row][d]
-        d++;
-    }
-    for(int row = matrix_2.number_of_rows; row > 0;row--;){
-        matrix_2_result_secondary_sum+= matrix_2.grid[row][d]
-        d--;
-    }
-    cout<< matrix_1_result_main_sum << "\n";
-    cout<< matrix_1_result_secondary_sum << "\n";
-    cout<< matrix_2_result_main_sum << "\n";
-    cout<< matrix_2_result_secondary_sum << "\n";
-    
-}
+	}
 
+}
 int main()
 {
-	string matrix_numbers_text_file_name;
 	cout<<"Hello Welcome to Matrix Operations and ETC." << "\n";
-	cout<<"Enter Matrix Numbers Text File Name: ";
-	cin >> matrix_numbers_text_file_name;
-	pair<Matrix,Matrix> matrices = create_matrices(matrix_numbers_text_file_name);
-	Matrix matrix1 = matrices.first;
-	Matrix matrix2 = matrices.second;
-	
+	choice_selection();
 	return 0;
 }
